efe61da915a20bc7d8f8dbcbdd29d82474722aaa
John Doe <204260440+cr0wSec@users.noreply.github.com>
2025-11-27 10:31:46 +0100

    setup rocket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+pub mod controllers;
+pub mod config;
+pub mod models;
+
+#[macro_use] extern crate rocket;
+
+use rocket::{Build, Rocket};
+use rocket_dyn_templates::Template;
+use crate::config::SERVER_CONFIG;
+use crate::controllers::ui::views::{get_index, get_login, get_register};
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+
+    let figment = rocket::Config::figment()
+        .merge(("secret_key", SERVER_CONFIG.secret_key.clone()));
+
+    rocket::custom(figment)
+        .mount("/", routes![get_index, get_login, get_register])
+        .attach(Template::fairing())
 }
