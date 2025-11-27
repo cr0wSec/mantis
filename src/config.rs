b8351bd01c8716cbafa2622858a60fc2d23947aa
John Doe <204260440+cr0wSec@users.noreply.github.com>
2025-11-27 10:56:00 +0100

    fix cargo fmt erros

--- src/config.rs
+++ src/config.rs
@@ -6,32 +6,33 @@ use once_cell::sync::Lazy;
 pub struct ServerConfig {
     pub crypto_pepper: Vec<u8>,
     pub secret_key: Vec<u8>,
-    pub connection: Mutex<PgConnection>
+    pub connection: Mutex<PgConnection>,
 }
 
 impl ServerConfig {
     fn new() -> Self {
-
         // load env variables
         dotenv().ok();
 
         // load crypto pepper var
         let crypto_pepper = dotenvy::var("CRYPTO_PEPPER")
-            .expect("CRYPTO_PEPPER is not set").into();
+            .expect("CRYPTO_PEPPER is not set")
+            .into();
 
         // load secret key var
         let secret_key = dotenvy::var("COOKIE_SECRET")
-            .expect("COOKIE_SECRET is not set").into();
+            .expect("COOKIE_SECRET is not set")
+            .into();
 
         // load postgres connection
-        let db_url= dotenvy::var("DATABASE_URL").expect("DATABASE_URL is not set");
+        let db_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL is not set");
         let connection = PgConnection::establish(&db_url)
             .unwrap_or_else(|_| panic!("Error connecting to {}", db_url));
 
         ServerConfig {
             crypto_pepper,
             secret_key,
-            connection: Mutex::new(connection)
+            connection: Mutex::new(connection),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-pub mod controllers;
 pub mod config;
+pub mod controllers;
 pub mod models;
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::{Build, Rocket};
-use rocket_dyn_templates::Template;
 use crate::config::SERVER_CONFIG;
 use crate::controllers::ui::views::{get_index, get_login, get_register};
+use rocket::{Build, Rocket};
+use rocket_dyn_templates::Template;
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-
-    let figment = rocket::Config::figment()
-        .merge(("secret_key", SERVER_CONFIG.secret_key.clone()));
+    let figment = rocket::Config::figment().merge(("secret_key", SERVER_CONFIG.secret_key.clone()));
 
     rocket::custom(figment)
         .mount("/", routes![get_index, get_login, get_register])
